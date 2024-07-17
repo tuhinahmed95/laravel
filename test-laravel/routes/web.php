cbb48d8b33d7af6ac5df4b57b950e6077feea453
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-17 17:09:59 +0600

    practice

--- test-laravel/routes/web.php
+++ test-laravel/routes/web.php
@@ -7,14 +7,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/post{id?}/comment{commentid}', function (string $id =null, string $comment = null) {
-    if($id){ 
-         return "<h1>Post Id : ". $id ."</h1> <h2>". $comment ."</h2>";
-    }else{ 
-        return "<h1>Not Found Id</h1>";
-    }
-});
+// Route::get('/post{id?}/comment{commentid}', function (string $id =null, string $comment = null) {
+//     if($id){ 
+//          return "<h1>Post Id : ". $id ."</h1> <h2> Second Post Id:". $comment ."</h2>";
+//     }else{ 
+//         return "<h1>Not Found Id</h1>";
+//     }
+// });
 
+Route::get('/post/{id}', function (string $id){ 
+    if($id){ 
+        return "<h1>Post Id : ". $id ."</h1>";
+    }else{ 
+        return "<h1>Post Id Not Found</h1>";
+    }
+})->whereNumber('id');
 
 
 // Route::view('post','/post');
