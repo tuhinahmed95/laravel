c72cdecf8de6e8e49a5783075377f80484fd2049
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-17 15:13:11 +0600

    practice

--- test-laravel/routes/web.php
+++ test-laravel/routes/web.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/post', function () {
-//     return view('post');
+// Route::get('/post {id}', function (string $id) {
+//     return "<h1>Post Id :". $id ." </h1>";
 // });
 
 // Route::view('post','/post');
 
-Route::get('/post/firstpost', function () {
-    return view('firstpost');
-});
+// Route::get('/post/firstpost', function () {
+//     return view('firstpost');
+// });
 
