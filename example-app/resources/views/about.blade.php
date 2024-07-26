31d4f267a334060d9142c7eeaf2b49c738be2560
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-26 17:01:48 +0600

    practie

--- example-app/resources/views/about.blade.php
+++ example-app/resources/views/about.blade.php
@@ -1,6 +1,2 @@
-@include('pages.header')
-<h1>About Page</h1>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi placeat tempora fuga harum mollitia temporibus facilis eaque saepe necessitatibus ducimus cupiditate, velit nisi doloribus, eius, eos tenetur voluptatum doloremque modi!</p>
-<a href="/">home</a>
-@include('pages.sidebar')
-@include('pages.footer')
+@extends('layouts.masterlayout')
+<h1>this about page</h1>
--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,7 +1,1 @@
-@include('pages.header')
-<h1>Home Page</h1>
-<a href="">home</a>
-<a href="/about">About</a>
-<a href="/post">Post</a>
-@include('pages.sidebar')
-@include('pages.footer')
+@extends('layouts.masterlayout')
