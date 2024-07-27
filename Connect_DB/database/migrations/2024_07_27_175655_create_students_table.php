5884b3a063bf6aaa56e52e0e0de31f6c1d796226
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-28 00:02:17 +0600

    migration database

---
+++ Connect_DB/database/migrations/2024_07_27_175655_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',30);
+            $table->string('email',40)->nullable()->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
