b8cd33f9176a6ad1d8a783e013ad5b5920c1029a
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-28 18:51:35 +0600

    new project practice

--- Project-Query/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ Project-Query/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('students', function (Blueprint $table) {
+        Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 30);
-            $table->string('email', 40)->nullable();
-            $table->timestamps();
-
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
         });
     }
 
@@ -29,8 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('students', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('failed_jobs');
     }
 };
