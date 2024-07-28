a09f5793d599040ff56812e1233943fe86b2c58d
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-28 16:22:25 +0600

    practice db

---
+++ Project-DB/database/seeders/studentSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\student;
+
+class studentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        student::create([ 
+            'name'=> 'Tanvir',
+            'email'=> 'tanvir@gmail.com'
+        ]);
+    }
+}
