42ba6c55fcce3b9d811ff6de32972717b6e2e7d7
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-28 18:15:39 +0600

    practice db

--- Project-DB/database/migrations/2024_07_28_120921_create_students_table.php
+++ Project-DB/database/migrations/2024_07_28_120921_create_students_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name', 30);
             $table->string('email', 40)->nullable();
             $table->timestamps();
+
         });
     }
 
