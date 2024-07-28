47459b067936e3d5865efc01a385bb2a3c145a43
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-28 23:09:29 +0600

    practice db

--- Project-Query/database/migrations/2024_07_28_170508_create_users_table.php
+++ Project-Query/database/migrations/2024_07_28_170508_create_users_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->string('email')->unique()->nullabale();
+            $table->integer('age');
+            $table->string('city');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +30,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
