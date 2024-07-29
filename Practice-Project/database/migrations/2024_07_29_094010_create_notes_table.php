5d3d3b45fd7643c1f01c90007c0a70b52925f88e
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-29 15:48:37 +0600

    practice

--- Practice-Project/database/migrations/2024_07_29_094010_create_notes_table.php
+++ Practice-Project/database/migrations/2024_07_29_094010_create_notes_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->longText('note');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('notes');
     }
 };
