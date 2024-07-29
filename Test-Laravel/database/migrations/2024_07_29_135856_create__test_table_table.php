4bed01542c41febf332d7b39528a8581558c3564
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-29 20:03:48 +0600

    test lar

--- Test-Laravel/database/migrations/2024_07_29_135856_create__test_table_table.php
+++ Test-Laravel/database/migrations/2024_07_29_135856_create__test_table_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('TestTable', function (Blueprint $table) {
-            //
+            $table->id();
+            $table->longText('note');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
         });
     }
 
