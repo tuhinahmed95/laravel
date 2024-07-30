843ff78bdc0262800e8f4372305b3650e7b9c4f7
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-30 13:36:55 +0600

    practice

--- POJECT-DB/database/migrations/2024_07_30_071726_create_users_table.php
+++ POJECT-DB/database/migrations/2024_07_30_071726_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('users');
     }
 };
