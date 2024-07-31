b08f3bb23af665f6be2c8787722178ebeb25a717
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-31 13:05:34 +0600

    practice db

--- Project-Query/resources/views/allusers.blade.php
+++ Project-Query/resources/views/allusers.blade.php
@@ -1,9 +1,44 @@
-<h1>Al Users List</h1>
-@foreach ($data as $id =>$user )
-    <h3> 
-        {{ $user ->name}} |
-        {{ $user ->email}} |
-        {{ $user ->age}} |
-        {{ $user ->city}} |
-    </h3>
-@endforeach
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container"> 
+        <div class="row"> 
+            <div class="col-6"> 
+                <h1>Al Users List</h1>
+
+                <table class="table table-borderd table-striped">
+                <tr>  
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Age</th>
+                    <th>City</th>
+                </tr>
+                    @foreach ($data as $id =>$user )
+
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->age}}</td>
+                        <td>{{$user->city}}</td>
+                        
+                    </tr>
+                    @endforeach
+
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+
+   
