72321517822dcbc6ac139878f9fcb90e4a4d26d1
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-04 18:56:18 +0600

    resource controller practice

---
+++ RS_CONTROLLER/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('Users', [UserController::class]);
