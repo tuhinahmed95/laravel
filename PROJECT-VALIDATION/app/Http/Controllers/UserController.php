654d5c00c0c210cbed63444f89cfddda22c3eced
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-04 14:23:24 +0600

    form validation pracitce

--- PROJECT-VALIDATION/app/Http/Controllers/UserController.php
+++ PROJECT-VALIDATION/app/Http/Controllers/UserController.php
@@ -12,9 +12,21 @@ class UserController extends Controller
         $req ->validate([ 
             'username'=>'required',
             'useremail'=>'required|email',
-            'userage'=>'required|numeric',
+            'password'=>'required|alpha_num|min:6',
+            'userage'=>'required|numeric|between:18,25',
             'usercity'=>'required',
-        ]);
+        ], 
+        [ 
+            'username.required'=> 'User Name is required',
+            'useremail.required'=> 'User Name is required',
+            'useremail.required'=> 'Enter Your Correct Email',
+            'password.required'=> 'User password is required',
+            'userage.required'=> 'User age is required',
+            'userage.numerice'=> 'User age must be numeric',
+            'userage.mi:18'=> 'User age must be 18',
+            'usercity.required'=> 'User city is required',
+        ]
+    );
 
         return $req->all();
     }
--- PROJECT-VALIDATION/resources/views/adduser.blade.php
+++ PROJECT-VALIDATION/resources/views/adduser.blade.php
@@ -12,38 +12,69 @@
         <div class="row"> 
             <div class="col-4"> 
                 <h1>Add New User</h1>
-                @if ($errors->any())
+                {{-- @if ($errors->any())
                     <ul class="alert alert-danger"> 
                         @foreach ($errors->all() as $erro)
                             <li>{{ $erro }}</li>
                         @endforeach
                     </ul>
-                @endif
+                @endif --}}
                 <form action=" {{ route('addUser') }} " method="POST"> 
                     @csrf
                     <div class="mb-3"> 
                         <label for="" class="form-label">Name</label>
-                        <input type="text" class="form-control" name="username">
+                        <input type="text" value="{{old('username')}}" class="form-control @error('username') is-invalid @enderror" name="username">
+                        <span class="text-danger"> 
+                            @error('username')
+
+                                {{$message}}
+                            @enderror
+                        </span>
                     </div>
 
                     <div class="mb-3"> 
                         <label for="" class="form-label">Email</label>
-                        <input type="email" class="form-control" name="useremail">
+                        <input value="{{old('useremail')}}" type="email" class="form-control @error('useremail') is-invalid @enderror" name="useremail">
+                        <span class="text-danger"> 
+                            @error('useremail')
+                                {{$message}}
+                            @enderror
+                        </span>
+                    </div>
+
+                    <div class="mb-3"> 
+                        <label for="" class="form-lable">Password</label>
+                        <input type="password" value="{{old('password')}}" class="form-control @error('password') is-invalid @enderror" name="password" >
+                        <span class="text-danger"> 
+                            @error('password')
+                                {{$message}}
+                            @enderror
+                        </span>
                     </div>
 
                     <div class="mb-3"> 
                         <label for="" class="form-label">Age</label>
-                        <input type="number" class="form-control" name="userage">
+                        <input type="number" value="{{old('userage')}}" class="form-control @error('userage') is-invalid @enderror" name="userage">
+                        <span class="text-danger"> 
+                            @error('userage')
+                                {{$message}}
+                            @enderror
+                        </span>
                     </div>
 
                     <div class="mb-3"> 
-                        <label for="" class="form-label">Name</label>
+                        <label for="" class="form-label">City</label>
                         <select class="form-control" name="usercity" > 
                             <option value="delli">Delli</option>
                             <option value="mumbai">Mumbai</option>
                             <option value="goa">Goa</option>
                             <option value="pune">Pune</option>
                         </select>
+                        <span> 
+                            @error('usercity')
+                                {{ $message }}
+                            @enderror
+                        </span>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
