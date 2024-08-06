fec16461fc070bfa90c238562168d3b681fa5c0b
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-06 14:50:00 +0600

    practice

--- Seeder-Practice/database/seeders/StudentSeeder.php
+++ Seeder-Practice/database/seeders/StudentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\student;
 
 class StudentSeeder extends Seeder
 {
@@ -14,6 +15,36 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        
+        $students = collect([ 
+            [ 
+                'name' => 'Tanvir',
+                'email' => 'tanvir@gmail.com',
+                'age' => 30,
+                'city' => 'Bhola'
+            ],
+            [ 
+                'name' => 'Shoain',
+                'email' => 'shoaib@gmail.com',
+                'age' => 55,
+                'city' => 'Rawalpindi'
+            ],
+            [ 
+                'name' => 'Amir',
+                'email' => 'amir@gmail.com',
+                'age' => 33,
+                'city' => 'Pakistan'
+            ]
+        ]);
+
+        $students -> each(function($student){ 
+            student::insert($student);
+        });
+
+        // student::create([ 
+        //     'name'=>'Tuhin',
+        //     'email'=>'mtuhin@gmail.com',
+        //     'age'=>30,
+        //     'city'=>'Gaibandha'
+        // ]);
     }
 }
