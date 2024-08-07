f52d7f2a1d83a43c2a61f145f90f0b27a1dbee72
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-07 12:37:19 +0600

    practice eloquent

--- PROJECT-ELOQUENT/app/Http/Controllers/UserController.php
+++ PROJECT-ELOQUENT/app/Http/Controllers/UserController.php
@@ -14,7 +14,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        
+        return view('welcome', compact('users'));
     }
 
     /**
