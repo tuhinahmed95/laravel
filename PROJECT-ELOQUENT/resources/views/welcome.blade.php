61fcde9c199ae88987ccd2d8099d136adb0e5fe9
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-07 16:01:24 +0600

    practice eloqouent

--- PROJECT-ELOQUENT/resources/views/welcome.blade.php
+++ PROJECT-ELOQUENT/resources/views/welcome.blade.php
@@ -11,18 +11,38 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-7">
-                <h1 class="bg-primary text-white text-center">User Information</h1>
-                <table class="table table-striped table-bordered mt-5">
+            <div class="col-8 bg-warning mb-3"> 
+                <h4>User Information</h4>
+            </div>
+            <div class="col-8">
+                <a href="" class="btn btn-success btn-sm mb-3">Add New</a>
+                <table class="table table-striped table-bordered">
+
+                    <tr> 
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>City</th>
+                        <th>View</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+
                     @foreach ($users as $user )
                         <tr> 
+                            <td>{{ $user->id}}</td>
                             <td>{{ $user->name}}</td>
                             <td>{{ $user->email}}</td>
                             <td>{{ $user->age}}</td>
                             <td>{{ $user->city}}</td>
+                            <td><a href="" class="btn btn-success">View</a></td>
+                            <td><a href="" class="btn btn-warning">Update</a></td>
+                            <td><a href="" class="btn btn-danger">Delete</a></td>
                         </tr>
                     @endforeach
                 </table>
+                
             </div>
         </div>
     </div>
