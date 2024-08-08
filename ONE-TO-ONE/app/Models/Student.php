85eb9552a26eea5d6b5d7fbf7044ce40e017cd76
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-08 17:31:37 +0600

    practice db

---
+++ ONE-TO-ONE/app/Models/Student.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    public function contact(){ 
+        return $this->hasOne(Contact::class);
+    }
+}
