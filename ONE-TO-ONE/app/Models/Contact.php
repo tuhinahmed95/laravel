033602efa5d8ce34ae580be0540d6361c3c5423e
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-08 21:43:48 +0600

    practiec

--- ONE-TO-ONE/app/Models/Contact.php
+++ ONE-TO-ONE/app/Models/Contact.php
@@ -9,7 +9,6 @@ class Contact extends Model
 {
     use HasFactory;
     protected $guarded = [];
-    public $timestamps = false;
 
     public function student(){ 
         return $this->belongsTo(Student::class);
