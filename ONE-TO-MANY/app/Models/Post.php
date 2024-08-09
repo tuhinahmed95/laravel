5c206557ea3179a11e9731aa69e4db3a4308cc78
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-09 15:06:16 +0600

    practice relation

--- ONE-TO-MANY/app/Models/Post.php
+++ ONE-TO-MANY/app/Models/Post.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Post extends Model
 {
     use HasFactory;
 
-    public function post(){ 
-        return $this->hasMany(Post::class);
+    public function user(){ 
+        return $this->hasMany(User::class);
     }
 }
--- ONE-TO-MANY/routes/web.php
+++ ONE-TO-MANY/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('/user', UserController::class);
+
+Route::resource('/post', PostController::class);
