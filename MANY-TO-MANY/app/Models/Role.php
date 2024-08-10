ce19141308e52f9f1ad010382a3bb90d4419ca4f
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-10 11:53:19 +0600

    practice many to many relation

--- MANY-TO-MANY/app/Models/Role.php
+++ MANY-TO-MANY/app/Models/Role.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Role extends Model
 {
     use HasFactory;
 
-    public function roles(){ 
-        return $this->belongsToMany(Role::class, 'user_role');
+    public function users(){ 
+        return $this->belongsToMany(Users::class, 'user_role');
     }
 }
