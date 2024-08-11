4751b8b9559880db900d475d9227db8c2dd2cb87
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-11 10:43:35 +0600

    practice hasone relation

--- HAS-ONE-THROUGH/database/migrations/2024_08_11_043420_create_companies_table.php
+++ HAS-ONE-THROUGH/database/migrations/2024_08_11_043420_create_companies_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->foreignId('user_id')->references('id')->on('users');
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('companies');
     }
 };
--- HAS-ONE-THROUGH/database/migrations/2024_08_11_044253_create_phone_number_table.php
+++ HAS-ONE-THROUGH/database/migrations/2024_08_11_044253_create_phone_number_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('phone_number', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id();
+            $table->id();
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('phone_number');
     }
 };
