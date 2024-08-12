d8dda8fa9b95873ade1611271b1a68e6234ad06c
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-13 00:28:01 +0600

    auth

---
+++ laravel_online_shop/app/Http/Middleware/AdminRedirectIfAuthenticated.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminRedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  string|null  ...$guards
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, ...$guards)
+    {
+        if (Auth::guard('admin')->check()) {
+            // return redirect(RouteServiceProvider::HOME);
+        }
+        return $next($request);
+    }
+}
