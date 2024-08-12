6c3ddcf97c0a3a304b2397a81198159aa9d7faae
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-12 23:11:58 +0600

    admin login

--- laravel_online_shop/routes/web.php
+++ laravel_online_shop/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminloginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login', [AdminloginController::class, 'index'])->name('admin.login');
