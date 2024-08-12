5e61307356af27b1c69fb1e81f87b8a3963327da
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-12 11:26:17 +0600

    pos

---
+++ Pos_Project/app/Models/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+    protected $primaryKey = 'id';
+    protected $fillable = ['cat_name', 'status'];
+    use HasFactory;
+}
