4746dedf89fcca7e642ff25d83f02bdbea58ea42
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-21 19:36:52 +0600

    practice poly

--- one-two-one-poly/app/Models/Image.php
+++ one-two-one-poly/app/Models/Image.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Image extends Model
 {
     use HasFactory;
 
-    public function image(){ 
-        return $this->morphOne(Image::class, 'imageable');
+    public function imageable(){ 
+        return $this->morphTo();
     }
 }
