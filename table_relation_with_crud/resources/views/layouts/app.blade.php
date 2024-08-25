90e18df9dbb28e5831eed9ff366ddb4246b8d267
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-25 11:21:51 +0600

    practice

--- table_relation_with_crud/resources/views/layouts/app.blade.php
+++ table_relation_with_crud/resources/views/layouts/app.blade.php
@@ -5,15 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
 
     <div class="container">
-        <div class="row">
+        <div class="row" >
             <div class="col-sm 8">
-                <h3>Product</h3>
+                <h3 class="bg-info">Product</h3>
+            </div>
+
+            <div class="text-align-end bg-success d-inline">
+                <h2>Add New Product</h2>
             </div>
         </div>
+
+        @yield('content')
     </div>
 
 </body>
