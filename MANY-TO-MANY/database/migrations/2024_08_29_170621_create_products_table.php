21b8d8a6dbdb1c3492a08bb9c1720a26e730ceed
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-29 23:11:48 +0600

    practice

--- MANY-TO-MANY/database/migrations/2024_08_29_170621_create_products_table.php
+++ MANY-TO-MANY/database/migrations/2024_08_29_170621_create_products_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('quantity');
+            $table->integer('price');
             $table->timestamps();
         });
     }
