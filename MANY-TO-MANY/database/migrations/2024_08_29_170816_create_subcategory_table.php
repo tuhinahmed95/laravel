9994613a0a2a271ebb4928dd8af6c58e7c209d15
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-08-29 23:42:48 +0600

    practic

--- MANY-TO-MANY/database/migrations/2024_08_29_170816_create_subcategory_table.php
+++ MANY-TO-MANY/database/migrations/2024_08_29_170816_create_subcategory_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('subcategory', function (Blueprint $table) {
+        Schema::create('subcategorys', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('category_id');
