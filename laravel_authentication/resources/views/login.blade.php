265a15181af48737a8167f762ee78ae1b12d329e
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-02 23:55:00 +0600

    practice auth

--- laravel_authentication/resources/views/login.blade.php
+++ laravel_authentication/resources/views/login.blade.php
@@ -7,6 +7,31 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-body">
+                            <form action="" method="POST">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input type="password" class="form-control" id="password" name="password">
+                                </div>
+                                <button type="submit" class="btn btn-success">Login</button>
+                                <a href="/" class="btn btn-primary">Back</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
