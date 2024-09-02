c845398912d7286bf06c5a2d3c4a209d34519030
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-02 22:55:28 +0600

    practice authentication

---
+++ laravel_authentication/resources/views/registration.blade.php
@@ -0,0 +1,30 @@
+<
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-5 mt-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Registration Form</h3>
+                        <div class="card-body">
+                            <form action="" method="POST">
+                                @csrf
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
