c9197b6a85668f6a92ab10b99a93bb523e19ac83
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-13 19:12:20 +0600

    practice laravel

--- laravel-practice-authentication/resources/views/blog.blade.php
+++ laravel-practice-authentication/resources/views/blog.blade.php
@@ -8,12 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<div class="container"></div>
-    <div class="row">
-        <div class="col-sm-12">
-            <table class="table"></table>
-        </div>
-    </div>
-</div>
+<h1>Blog page</h1>
 </body>
 </html>
