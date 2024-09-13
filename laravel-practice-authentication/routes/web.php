1d13f09a2729efc5bc7133d17344b7cc0cf1d287
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-13 19:10:50 +0600

    practice laravel

--- laravel-practice-authentication/routes/web.php
+++ laravel-practice-authentication/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/page',[BlogController::class, 'index']);
