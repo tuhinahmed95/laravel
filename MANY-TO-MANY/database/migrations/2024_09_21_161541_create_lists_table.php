786830158fd0726390e6ceb6a98ac592272df13d
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-21 22:25:00 +0600

    laravel

---
+++ MANY-TO-MANY/database/migrations/2024_09_21_161541_create_lists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->string('price');
+            $table->unsignedBigInteger('categorys_id')->on('categories')->oncascade();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lists');
+    }
+};
