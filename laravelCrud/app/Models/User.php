da0f013db1cb9585d15a8901e6d175188eaa3bdc
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-26 19:06:49 +0600

    db crud

--- laravelCrud/app/Models/User.php
+++ laravelCrud/app/Models/User.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
-    public $timestamps = false;
+    // public $timestamps = true;
 }
--- laravelCrud/database/migrations/2024_09_26_052915_create_users_table.php
+++ laravelCrud/database/migrations/2024_09_26_052915_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email');
             $table->integer('age');
             $table->string('city');
+            $table->timestamps();
         });
     }
 
