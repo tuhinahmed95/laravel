d20d0d5b2bf25de0b93909eab26c9f43fa3339f4
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-28 12:10:42 +0600

    dbcrud

--- tablerelation/app/Http/Controllers/UserController.php
+++ tablerelation/app/Http/Controllers/UserController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function showPage(){
+        $users = DB::table('users')->get();
+
+        return view('allusers',compact('users'));
+    }
 }
