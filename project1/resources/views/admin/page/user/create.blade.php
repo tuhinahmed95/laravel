e7f698fff276cfb84a2111aec576f138f3e3a257
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-04 21:13:12 +0600

    auth

--- project1/resources/views/admin/page/user/create.blade.php
+++ project1/resources/views/admin/page/user/create.blade.php
@@ -1,4 +1,19 @@
 @extends('admin.partial.layout')
 @section('content')
-
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h3>User Create Form</h3>
+            </div>
+            <div class="card-body">
+                <form action="" method="POST">
+                    @csrf
+                    <label for="name" class="form-group">Name</label>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
