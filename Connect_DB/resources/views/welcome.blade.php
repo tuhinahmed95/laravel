ce4666b4dd983f6fe5a523772a62f7cbd79d5570
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-08 23:02:13 +0600

    laravel

--- Connect_DB/resources/views/welcome.blade.php
+++ Connect_DB/resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
@@ -28,6 +29,8 @@
 
                         <label for="password">Confirm Password</label>
                         <input type="text" id="password" class="form-control">
+
+                        <button type="submit" value="submit" class="btn btn-success">Submit</button>
                     </form>
                 </div>
             </div>
