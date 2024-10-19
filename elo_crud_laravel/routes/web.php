9bef5a1b5a598cd304ad0c14689a22da09d40b22
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-19 15:36:33 +0600

    laravel crud

--- elo_crud_laravel/routes/web.php
+++ elo_crud_laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/user',UserController::class);
