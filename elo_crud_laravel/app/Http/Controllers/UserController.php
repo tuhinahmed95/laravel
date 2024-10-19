80fa8e4d402cbd77c576c54069099e6f2852175e
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-19 17:13:32 +0600

    elo laravel crud

--- elo_crud_laravel/app/Http/Controllers/UserController.php
+++ elo_crud_laravel/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -11,7 +12,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        
+        $users = User::all();
+        return view('home',compact('users'));
     }
 
     /**
--- elo_crud_laravel/routes/web.php
+++ elo_crud_laravel/routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::resource('/user',UserController::class);
