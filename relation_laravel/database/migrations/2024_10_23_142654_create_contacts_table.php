59e1d117087d4b02cfcfc492726c4708d3fab878
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-23 20:40:23 +0600

    laravel

---
+++ relation_laravel/database/migrations/2024_10_23_142654_create_contacts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('phone');
+            $table->string('city');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
