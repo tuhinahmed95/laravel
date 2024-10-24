6e8c5152dfe04946793197f3e6c557d70f639e1f
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-24 20:18:13 +0600

    laravel

---
+++ relation_laravel/resources/views/viewtable.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Table Relation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="card">
+            <div class="card-header">
+                <h1 class="bg-success d-inline">Table Relation</h1>
+            </div>
+            <div class="card-body">
+                <table class="table table-striped">
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
