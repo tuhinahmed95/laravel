b33fcf513cfbde37fcc285068a1d878e60a0a03f
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-31 09:35:43 +0600

    laravel

---
+++ new_crud/resources/views/adduser.blade.php
@@ -0,0 +1,38 @@
+@extends('layout')
+
+@section('title')
+Add User
+@endsection
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <form action="#" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="age">Age</label>
+                <input type="number" name="age" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="city">City</label>
+                <input type="text" name="city" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <input type="submit" value="submit" class="btn btn-success">
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
