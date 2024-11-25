b26aa6ab8fbd53b857b2d031a647b774cfa933a1
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-25 10:05:02 +0600

    api

--- sanctom_api/app/Http/Controllers/Api/AuthController.php
+++ sanctom_api/app/Http/Controllers/Api/AuthController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Validator;
 class AuthController extends Controller
 {
     public function signup(Request $request){
-        $validateUser = Validator(
+        $validateUser = Validator::make(
             $request->all(),
             [
                 'name'=>['required'],
@@ -27,15 +27,35 @@ class AuthController extends Controller
                 ],401);
             }
 
-            $users = User::create([
+            $user = User::create([
                 'name'=>$request->name,
                 'email'=>$request->email,
                 'password'=>$request->password,
             ]);
+
+            return response()->json([
+                'status'=>true,
+                'message'=>'User Created Successfully',
+                'user'=>$user,
+            ],200);
     }
 
     public function login(Request $request){
+        $validateUser = validator::make(
+            $request->all(),
+            [
+                'email'=>['required|email'],
+                'password'=>['required'],
+            ]
+        );
 
+        if($validateUser->fails()){
+            return response()->json([
+                'status'=>false,
+                'message'=>'Authentication Fails',
+                'errors'=>$validateUser->errors()->all()
+            ],404);
+        }
     }
 
     public function logout(Request $request){
