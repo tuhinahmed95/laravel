8b87d71a8ce416a77c65e54ed4e528f123c01244
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-07 12:27:27 +0600

    larave

---
+++ auth_m_p/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header mt-3">
+                    <h1 class="mt-3">Home Page</h1>
+                </div>
+                <div class="card-body">
+                    <div>
+                        <a href="" class="btn btn-success">Registration</a>
+                        <a href="/" class="btn btn-danger">Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
