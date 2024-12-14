669c8b6998f7784b54d4f7043e1d15e3f0e13a5c
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-14 21:07:13 +0600

    laravel

--- project_A_C/app/Http/Controllers/UserController.php
+++ project_A_C/app/Http/Controllers/UserController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(){
+        return view('auth.register');
+    }
 }
--- project_A_C/routes/web.php
+++ project_A_C/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('auth.register','register');
+Route::post('registerMatch',[UserController::class,]);
