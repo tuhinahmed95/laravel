46e5f83ddfa6efb9edcae2fbd404532205adf266
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-16 19:57:10 +0600

    laravel

---
+++ image_crud/resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="text-center">File Upload</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <a href="" class="btn btn-success">Add New</a>
+            <form action="#" method="POST">
+                <div class="form-group">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" name="name" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="file" class="form-label">FileUpload</label>
+                    <input type="file" name="file" class="form-control">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
