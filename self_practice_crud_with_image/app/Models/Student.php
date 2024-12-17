fb9f1a617afa477793ad79d88ee71b4efeb4dd8f
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-17 20:43:36 +0600

    laravel

---
+++ self_practice_crud_with_image/app/Models/Student.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'age',
+        'image',
+        'city',
+    ];
+}
