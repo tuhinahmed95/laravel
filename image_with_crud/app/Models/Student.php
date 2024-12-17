e7ab266e20b77f38b94632e70b633abcd7c50e16
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-17 12:06:25 +0600

    laravel

--- image_with_crud/app/Models/Student.php
+++ image_with_crud/app/Models/Student.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'email',
+        'age',
+        'image'
+    ];
 }
--- image_with_crud/database/migrations/2024_12_17_055155_create_students_table.php
+++ image_with_crud/database/migrations/2024_12_17_055155_create_students_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->integer('age');
+            $table->string('image');
             $table->timestamps();
         });
     }
