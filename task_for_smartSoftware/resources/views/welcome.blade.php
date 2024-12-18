18649fcb3bc3886ebdcccb9df5c30de1f28a1d5b
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-18 12:03:41 +0600

    laravel

---
+++ task_for_smartSoftware/resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center mt-2 bg-success">Home Page</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="text-center">Home Page</h3>
+                </div>
+                <div class="card-body">
+                    <div>
+                        <a href="" class="btn btn-success">RegisteForm</a>
+                        <button class="btn btn-secondary">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
