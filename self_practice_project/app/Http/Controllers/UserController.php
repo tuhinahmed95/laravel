4457a60344ca53c31b1fefe9b18e2ee7c829bc53
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-23 13:58:39 +0600

    laravel

--- self_practice_project/app/Http/Controllers/UserController.php
+++ self_practice_project/app/Http/Controllers/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validate = $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required|confirmed',
+        ]);
+
+        User::create($validate);
+        return view('login');
+    }
 }
--- self_practice_project/routes/web.php
+++ self_practice_project/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('register','register')->name('register');
+Route::post('registerSave',[UserController::class,'register'])->name('register');
