220ad53b830f907f46ee7a1fdedf577509322d6a
tuhinahmed95 <mtuhinahmed95@gmail.com>
2025-01-03 15:15:11 +0600

    laravel

---
+++ calss_tahsan_sir_second/resources/views/layouts/admin.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Page</title>
+</head>
+<body>
+    <h3>Header Section</h3>
+
+    @section('content')
+    <h3>i come from dashboard</h3>
+    @endsection
+    <h3>Footer Section</h3>
+
+</body>
+</html>
