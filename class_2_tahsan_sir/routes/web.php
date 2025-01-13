d5b95c38daace55edeefaa7727e3b334307ca4a0
tuhinahmed95 <mtuhinahmed95@gmail.com>
2025-01-13 21:52:28 +0600

    laravel

--- class_2_tahsan_sir/routes/web.php
+++ class_2_tahsan_sir/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [HomeController::class,'dashboard']);
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
